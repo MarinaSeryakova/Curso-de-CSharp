bf55873e8cb5f616ea71b3114ce4b33106194e77
Borja Cabeza <borjacabeza@msn.com>
2019-03-01 13:58:08 +0100

    Día 01-mar-2019

--- Curso.CSharp.ConsoleAppEjercicios/Program.cs
+++ Curso.CSharp.ConsoleAppEjercicios/Program.cs
@@ -10,7 +10,7 @@ namespace Curso.CSharp.ConsoleAppEjercicios
     {
         static void Main(string[] args)
         {
-            Arrays.Ejercicio1();
+            Interfaces.Ejercicio1();
 
             Console.ReadKey();
         }
@@ -18,6 +18,7 @@ namespace Curso.CSharp.ConsoleAppEjercicios
 
     public static class Arrays
     {
+        //Calculo de la letra NIF o NIE
         public static void Ejercicio1()
         {
             string[] letras = new string[] { "T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E", "T" };
@@ -35,19 +36,19 @@ namespace Curso.CSharp.ConsoleAppEjercicios
                     case "X":
                         dni = dni.Substring(1, 7);
                         Console.WriteLine(int.TryParse(dni, out numero)
-                            ? "El NIF es X" + numero.ToString() + letras[numero % 23]
+                            ? "El NIE es X" + numero.ToString() + letras[numero % 23]
                             : "Error al procesar el NIE");
                         break;
                     case "Y":
                         dni = dni.Substring(1, 7);
                         Console.WriteLine(int.TryParse(dni, out numero)
-                            ? "El NIF es Y" + numero.ToString() + letras[(10000000 + numero) % 23]
+                            ? "El NIE es Y" + numero.ToString() + letras[(10000000 + numero) % 23]
                             : "Error al procesar el NIE");
                         break;
                     case "Z":
                         dni = dni.Substring(1, 7);                        
                         Console.WriteLine(int.TryParse(dni, out numero)
-                            ? "El NIF es Z" + numero.ToString() + letras[(20000000 +numero) % 23]
+                            ? "El NIE es Z" + numero.ToString() + letras[(20000000 +numero) % 23]
                             : "Error al procesar el NIE");
                         break;
                     default:
@@ -62,6 +63,70 @@ namespace Curso.CSharp.ConsoleAppEjercicios
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("Error: El DNI tiene más de 8 caracteres.");
             }
+        }
+
+        //Rellenamos un array con 100 números aleatorios
+        //Retorna Suma Total, Media, Suma Pares, Suma Impares, Min y Max
+        public static void Ejercicio2()
+        {
+            int[] array = new int[100];
+            Random rnd = new Random();
+            long pares = 0; 
+            long impares = 0;
+
+            for (var i = 0; i < 100; i++)
+            {
+                int numero = rnd.Next(100);
+
+                if (numero % 2 == 0) pares += (long)numero;
+                else impares += numero;
+
+                array[i] = numero;
+            }
+
+            Console.Clear();
+            Console.WriteLine("Suma Total:".PadRight(20, ' ') + array.Sum().ToString("N0").PadLeft(10, ' '));
+            Console.WriteLine("Media:".PadRight(20, ' ') + array.Average().ToString("N0").PadLeft(10, ' '));
+            Console.WriteLine("Suma Pares:".PadRight(20, ' ') + pares.ToString("N0").PadLeft(10, ' '));
+            Console.WriteLine("Suma Impares:".PadRight(20, ' ') + impares.ToString("N0").PadLeft(10, ' '));
+            Console.WriteLine("Min:".PadRight(20, ' ') + array.Min().ToString("N0").PadLeft(10, ' '));
+            Console.WriteLine("Max:".PadRight(20, ' ') + array.Max().ToString("N0").PadLeft(10, ' '));  
+        }
+
+        //Repite el ejercicio anterior con un Do-While
+        public static void Ejercicio3()
+        {
+            int[] array = new int[100];
+            Random rnd = new Random();
+            long pares = 0;
+            long impares = 0;
+            int contador = 0;
+
+            do
+            {
+                int numero = rnd.Next(100);
+
+                if (numero % 2 == 0) pares += (long)numero;
+                else impares += numero;
+
+                array[contador] = numero;
+
+                contador++;
+            } while (contador < 100);
+
+            Console.Clear();
+            Console.WriteLine("Suma Total:".PadRight(20, ' ') + array.Sum().ToString("N0").PadLeft(10, ' '));
+            Console.WriteLine("Media:".PadRight(20, ' ') + array.Average().ToString("N0").PadLeft(10, ' '));
+            Console.WriteLine("Suma Pares:".PadRight(20, ' ') + pares.ToString("N0").PadLeft(10, ' '));
+            Console.WriteLine("Suma Impares:".PadRight(20, ' ') + impares.ToString("N0").PadLeft(10, ' '));
+            Console.WriteLine("Min:".PadRight(20, ' ') + array.Min().ToString("N0").PadLeft(10, ' '));
+            Console.WriteLine("Max:".PadRight(20, ' ') + array.Max().ToString("N0").PadLeft(10, ' '));
+
+
+
+
+
+
         }
     }
 
@@ -101,6 +166,23 @@ namespace Curso.CSharp.ConsoleAppEjercicios
             {
                 Console.WriteLine("{0}", i);
             }
+        }
+    }
+
+    public static class Interfaces
+    {
+        //Creamos la Interfaz IVehiculo 
+        //Dos o tres propiedades (velocidad, color, marca) y el método PintarDatos()
+
+        //Creamos las clases Coche (ruedas), Barco (cubiertas) y Avión(motores) implementando IVehiculo 
+        //con una propiedad expecifica para cada clases.
+
+        //Implementa PintarDatos utilizando propiedades comunes y expecificas.
+
+        //Implementa el método para la IVehiculo en cada uno de los objetos.
+        public static void Ejercicio1()
+        {
+
         }
     }
 }
