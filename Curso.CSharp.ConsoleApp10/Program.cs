1878d609074a62356e51ce6147a963893366b07a
Borja Cabeza <borjacabeza@msn.com>
2019-03-07 14:30:33 +0100

    07-mar-2019

--- Curso.CSharp.ConsoleApp10/Program.cs
+++ Curso.CSharp.ConsoleApp10/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Curso.CSharp.ConsoleApp10.Models;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Curso.CSharp.ConsoleApp10
 {
@@ -12,8 +14,126 @@ namespace Curso.CSharp.ConsoleApp10
         static void Main(string[] args)
         {
             Console.Clear();
-
             ModelNorthwind db = new ModelNorthwind();
+
+            //Consulta con ADO.NET
+            //=======================================================================
+            //
+            // a. Creamos una cadena de conexión
+
+            SqlConnectionStringBuilder csb = new SqlConnectionStringBuilder();
+            csb.DataSource = "LOCALHOST";
+            csb.InitialCatalog = "NORTHWIND";
+            csb.UserID = "";
+            csb.Password = "";
+            csb.IntegratedSecurity = true;
+
+            string cadenaConexion = csb.ToString();
+
+            // b. Creamos un objeto de conexión
+
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+            cn.Open();
+
+            // c. Creamos un objeto Comando
+
+            SqlCommand cm = new SqlCommand();
+            cm.Connection = cn;
+            cm.CommandText = "SELECT * FROM dbo.Customers WHERE Country = 'Spain'";
+
+            //Utilizamos ExecuteNonQuery() para INSERT, UPDATE, DELETE
+            //Retirna un int con el número de registro afectados, -1 Error
+            //cm.ExecuteNonQuery();
+
+            //Utilizamos ExecuteReader() para SELECT
+            SqlDataReader reader = cm.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0} {1}", 
+                        reader["CompanyName"].ToString(), reader["Country"].ToString());
+                }
+
+                reader.Close();        
+            }
+            else
+            {
+                Console.WriteLine("No se han encontrado registros.");
+            }
+
+            cm.Dispose();
+            cn.Close();
+            cn.Dispose();
+
+            Console.ReadKey();
+
+
+            //Eliminar un registro
+            var region3 = db.Region.Where(r => r.RegionID == 10).FirstOrDefault();
+            db.Region.Remove(region3);
+
+            db.SaveChanges();
+
+            Console.WriteLine("Registro Eliminado Correctamente.");
+            Console.ReadKey();
+
+            //Modificar un registro
+            var region2 = db.Region.Where(r => r.RegionID == 10).FirstOrDefault();
+            region2.RegionDescription = region2.RegionDescription.Trim() +  "aaaaaaEl inframundo";
+
+            db.SaveChanges();
+
+            Console.WriteLine("Registro Modificado Correctamente.");
+            Console.ReadKey();
+
+            //Insertar un registro
+            Region region = new Region();
+            region.RegionID = 10;
+            region.RegionDescription = "Triangulo de las Bermudas";
+
+            db.Region.Add(region);
+            db.SaveChanges();
+
+            Console.WriteLine("Registro Insertado Correctamente.");
+            Console.ReadKey();
+            
+            //LINQPad5
+
+            //Demo SQLQuery
+            var e1 = db.Customers.ToList();
+            var e1b = db.Customers.SqlQuery("SELECT * FROM Customers");
+
+            var e2 = db.Customers.Where(r => r.Country == "Mexico").ToList();
+            var e2b = db.Customers.SqlQuery("SELECT * FROM Customers WHERE Country = 'Mexico'");
+
+
+            //Empleados con clientes que viven en su misma ciudad
+            var EmpleadosClientes = db.Employees
+                .Select(r => new { r.FirstName, r.LastName, r.City,
+                    clientes = db.Customers.Where(c => c.City == r.City).ToList() })
+                .Where(r => r.clientes.Count() > 0)
+                .ToList();
+
+
+            foreach (var empleado in EmpleadosClientes)
+            {
+                Console.WriteLine("{0} {1} - {2}", empleado.FirstName, empleado.LastName, empleado.City);
+                Console.WriteLine("===========================================================");
+                foreach (var cliente in empleado.clientes)
+                {
+                    Console.WriteLine("  {0}", cliente.CompanyName);
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+
+            //Empleados y número de productos diferentes vendidos
+            //Pendiente de Revisión
+
 
             //Clientes que no han comprado nada
             var clientesOut = db.Customers
